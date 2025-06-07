d0cdb638b037166608173595cddb27bce59a0540
rizkimuliono <rizkimuliono@gmail.com>
2025-06-07 22:05:58 +0700

    user_id migration tabel transactions

--- database/migrations/2025_05_27_030210_transactions.php
+++ database/migrations/2025_05_27_030210_transactions.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('book_id')->constrained('books')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->text('keterangan');
             $table->integer('status');
             $table->decimal('biaya', 10, 2);
