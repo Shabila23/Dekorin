6d8ff7abf65e38f71025362351e319f30cdffe05
rizkimuliono <rizkimuliono@gmail.com>
2025-06-12 09:07:01 +0700

    nambahin ganteng

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('template')
 @section('title', 'Dashboard')
 @section('content')
-    <h2>Selamat Datang Admin</h2>
+    <h2>Selamat Datang Admin, Ganteng!</h2>
     <p>Ini adalah halaman dashboard untuk admin.</p>
 @endsection
